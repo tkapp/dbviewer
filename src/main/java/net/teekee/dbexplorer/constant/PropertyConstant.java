2b31631f871fde9eced278ed10610df95f39c2f1
takano <takano@localhost>
2018-03-01 12:36:31 +0900

    ver 0.0.1

--- src/main/java/net/teekee/dbexplorer/constant/PropertyConstant.java
+++ src/main/java/net/teekee/dbexplorer/constant/PropertyConstant.java
@@ -2,7 +2,7 @@ package net.teekee.dbexplorer.constant;
 
 public class PropertyConstant {
 
-  public static String DB = "db.properties";
+  public static String DB = "db";
 
   public static String CONTEXTS = "contexts";
 }
